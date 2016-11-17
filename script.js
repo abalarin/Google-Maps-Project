2854cd7bde2c881f9a6e976f209a0661c978bb24
Austin Balarin <austin@Austins-MacBook-Pro.local>
2016-11-16 21:42:51 -0500

    center map on address click

--- script.js
+++ script.js
@@ -1,34 +1,57 @@
+var i = 0;
+var locationList = []
 $(document).ready(function() {
-  //$('button').click(function() {
-  $('form').submit(function(e) {
-    e.preventDefault();
+  //$('button').click(function(e) {}
+  function run(e){
+    type = e.originalEvent.type;
+    if (type == 'submit') {
+      e.preventDefault();
+    }
+
     var address = $('input:first').val();
     if(address){
       var jax = $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=?' + address);
       jax.done(function(data){
         var location_json = data.results[0];
         addPoint(location_json.geometry.location);
-        $('<li>')
-            .append($('<a>').attr('href','/user/messages'))
-            .addClass('address')
+        locationList.push(location_json.geometry.location);
+        console.log(locationList[i]);
+
+        $('<li>').addClass('address address' + i).appendTo('ul');
+
+        $('<label>')
+            .addClass('address address' + i)
             .text(location_json.formatted_address)
-            .appendTo('ul');
+            .attr("onclick", "centerMap(" + i +")")
+            .appendTo('.address' + i);
+        i++;
       })
     }
     else {
       $( "span" ).text( "Invalid Address..." ).show().fadeOut(1000);
     }
-  });
+  }
+  $('form').submit(function(e) { run(e); });
+
+  $('button').click(function(e) { run(e); });
+
+  $('<li>').on("click",function() { console.log(worked); });
+
 });
 
 var map;
 function initMap(location) {
-  if(!location)
+  if(!location){
     location = {lat: 41.08394699999999, lng: -74.176609}
-  map = new google.maps.Map(document.getElementById('map'), {
+  }
+    map = new google.maps.Map(document.getElementById('map'), {
     center: location,
     zoom: 10
   });
+}
+function centerMap(location) {
+  map.setCenter(locationList[location]);
+
 }
 
 function addPoint(location) {
